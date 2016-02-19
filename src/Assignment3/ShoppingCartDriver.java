56c4e0bc3a7df3bb9469b635189b90a434c6a78f
michaelspearing <MSpearing@UTexas.edu>
2016-02-19 15:19:50 -0600

    Initial Commit - Michael

--- src/Assignment3/ShoppingCartDriver.java
+++ src/Assignment3/ShoppingCartDriver.java
@@ -7,6 +7,7 @@ public class ShoppingCartDriver
 
 	  public static void main(String[] args) 
 	  {
+		  System.out.println("??");
 		// TODO Auto-generated method stub
 		
 		//Open file; file name specified in args (command line)
